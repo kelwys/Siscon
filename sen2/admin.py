a62f58b1de1dcf7ec18faff19c73062dc728f28b
Kelwy Oliveira <kelwys@ucl.br>
2015-10-29 20:11:28 -0200

    add models sen

---
+++ sen2/admin.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from django.contrib import admin
+from sen.models import *
+
+admin.site.register(TipoSensor)
+admin.site.register(UnidadeMedida)
+admin.site.register(Sensor)
