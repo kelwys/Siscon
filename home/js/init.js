7b08fe9142381cfc78684dd8f1b1a3584b240ffd
Kelwy Oliveira <kelwys@ucl.br>
2015-11-26 13:03:36 -0200

    Add subprojeto (pagina inicial do sistema)

---
+++ home/js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav({'edge': 'left'});
+	$( document ).ready(function(){})
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
